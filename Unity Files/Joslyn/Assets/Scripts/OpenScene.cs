cdf76de5d0cd0cf3d11f430f5f8854e442bb9296
Joseph <joseph@diggablegames.com>
2017-05-12 14:24:10 -0700

    Re-organizing Files for two other scenes

---
+++ Unity Files/Joslyn/Assets/Scripts/OpenScene.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class OpenScene : MonoBehaviour {
+	[SerializeField] string sceneName;
+
+	public void openScene(){
+		SceneManager.LoadScene(sceneName);
+	}
+}
